ea4a9a37750a592e09d0cc0213886afdaccc127c
Twixenn <fiagustafsson1998@gmail.com>
2017-09-18 11:34:05 +0200

    Länkade bootstrap i index.php och la till menyn

--- components/menu.php
+++ components/menu.php
@@ -1,1 +1,13 @@
-
+<nav class="navbar navbar-default navbar-fixed-top">
+  <div class="container">
+    <div class="collapse navbar-collapse">
+      <ul>
+        <li><a href="#">Hejsan</a></li>
+        <li><a href="#">Hejsan</a></li>
+        <li><a href="#">Hejsan</a></li>
+        <li><a href="#">Hejsan</a></li>
+        <li><a href="#">Hejsan</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
