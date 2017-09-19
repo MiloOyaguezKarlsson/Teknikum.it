2a7a3bfa8d232fadcb0bf6d0f619b94df458b65e
MiloOyaguezKarlsson <mioy97001@utb.vaxjo.se>
2017-09-19 22:38:33 +0200

    footer

---
+++ components/footer.php
@@ -0,0 +1,53 @@
+<style>
+    footer{
+        background-color: #012E4D;
+        color: white;
+    }
+    .footer-lista{
+        list-style: none;
+    }
+    .align-right{
+        text-align: right;
+    }
+    footer h3{
+        border-bottom: #F4512E;
+    }
+</style>
+<link rel="stylesheet" href="../bower_components/bootstrap/dist/css/bootstrap.min.css">
+
+<footer>
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-3">
+                <img src="../bilder/teknikum_it_logga.jpg" alt="Teknikum.it logo" height="150px" width="200px" class="img-rounded"/>
+            </div>
+            <div class="col-sm-3">
+                <h3>Adress</h3>
+                <ul class="footer-lista">
+                    <li>IT-utb, B-korridoren</li>
+                    <li>Gamla Norrvägen 6-8</li>
+                    <li>352 43 VÄXJÖ</li>
+                </ul>
+            </div>
+            <div class="col-md-3">
+                <h3>Webbsidor</h3>
+                <ul class="footer-lista">
+                    <li><a href="http://www.teknikum.it">http://www.teknikum.it</a></li>
+                    <li><a href="http://www.teknikum.se">http://www.teknikum.se</a></li>
+                    <li><a href="http://www.iteducation.nu">http://www.iteducation.nu</a></li>
+                </ul>
+            </div>
+            <div class="col-lg-3">
+                <h3>E-post</h3>
+                <ul class="footer-lista">
+                    <li><a href="mailto:lars.mattson@teknikum.it">admin@teknikum.it</a></li>
+                    <li><a href="mailto:lars.mattson@teknikum.it">webbmaster@teknikum.it</a></li>
+                    <li><a href="mailto:lars.mattson@teknikum.it">abuse@teknikum.it</a></li>
+
+                </ul>
+
+            </div>
+        </div>
+        <p class="align-right">©2017 Teknikum.IT</p>
+    </div>
+</footer>
