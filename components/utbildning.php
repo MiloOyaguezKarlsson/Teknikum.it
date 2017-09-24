e502f0f51885d8af8c57ee57e15ebf80a89efb89
Twixenn <fiagustafsson1998@gmail.com>
2017-09-24 17:02:38 +0200

    Lagt in ikoner för utbildningsområdena

--- components/utbildning.php
+++ components/utbildning.php
@@ -1,6 +1,6 @@
 <style>
 section.part {
-  padding: 30px 0;
+  padding: 40px 0;
 }
 
 h1 {
@@ -18,12 +18,16 @@ h1 {
   height: 4px;
   width: 70px;
   margin: auto;
-  margin-bottom: 30px;
+  margin-bottom: 40px;
 }
 
 .thumbnail {
   background-color: transparent !important;
   border: none !important;
+}
+
+.thumbnail img {
+  margin-bottom: 10px;
 }
 
 .område {
@@ -43,69 +47,69 @@ h1 {
       <div class="row">
         <div class="col-xs-3 col-md-2 område">
           <div class="thumbnail">
-            <img src="bilder/teknikum.it-small.svg" alt="">
-            <p>Lorem Ipsum</p>
+            <img src="bilder/pc.svg" alt="">
+            <p>Grundläggande PC-teknik</p>
           </div>
         </div>
 
         <div class="col-xs-3 col-md-2 område">
           <div class="thumbnail">
-            <img src="bilder/teknikum.it-small.svg" alt="">
-            <p>Lorem Ipsum</p>
-            <p>Lorem Ipsum</p>
-            <p>Lorem Ipsum</p>
+            <img src="bilder/windows.svg" alt="">
+            <p>Windows Server 2012R2</p>
+            <p>Windows Server 2016</p>
+            <p>Windows 7 och 10</p>
           </div>
         </div>
 
         <div class="col-xs-3 col-md-2 område">
           <div class="thumbnail">
-            <img src="bilder/teknikum.it-small.svg" alt="">
-            <p>Lorem Ipsum</p>
+            <img src="bilder/itil.svg" alt="">
+            <p>ITIL</p>
           </div>
         </div>
 
         <div class="col-xs-3 col-md-2 område">
           <div class="thumbnail">
-            <img src="bilder/teknikum.it-small.svg" alt="">
-            <p>Lorem Ipsum</p>
+            <img src="bilder/net.svg" alt="">
+            <p>.NET</p>
           </div>
         </div>
 
         <div class="col-xs-3 col-md-2 område">
           <div class="thumbnail">
-            <img src="bilder/teknikum.it-small.svg" alt="">
-            <p>Lorem Ipsum</p>
+            <img src="bilder/html-css.svg" alt="">
+            <p>HTML/CSS</p>
           </div>
         </div>
 
         <div class="col-xs-3 col-md-2 område">
           <div class="thumbnail">
-            <img src="bilder/teknikum.it-small.svg" alt="">
-            <p>Lorem Ipsum</p>
-            <p>Lorem Ipsum</p>
-            <p>Lorem Ipsum</p>
-            <p>Lorem Ipsum</p>
+            <img src="bilder/cisco.svg" alt="">
+            <p>Cisco IT Essentials</p>
+            <p>Cisco Switching and Routing</p>
+            <p>Cisco Security</p>
+            <p>Cisco Intro to Network</p>
           </div>
         </div>
 
         <div class="col-xs-3 col-md-2 område">
           <div class="thumbnail">
-            <img src="bilder/teknikum.it-small.svg" alt="">
-            <p>Lorem Ipsum</p>
+            <img src="bilder/iso.svg" alt="">
+            <p>Informationssäkerhet ISO 27000</p>
           </div>
         </div>
 
         <div class="col-xs-3 col-md-2 område">
           <div class="thumbnail">
-            <img src="bilder/teknikum.it-small.svg" alt="">
-            <p>Lorem Ipsum</p>
+            <img src="bilder/php.svg" alt="">
+            <p>PHP</p>
           </div>
         </div>
 
         <div class="col-xs-3 col-md-2 område">
           <div class="thumbnail">
-            <img src="bilder/teknikum.it-small.svg" alt="">
-            <p>Lorem Ipsum</p>
+            <img src="bilder/mobil.svg" alt="">
+            <p>Apputveckling</p>
           </div>
         </div>
 
