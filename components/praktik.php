bba5ef949825f977affc44bd82454f29584f1089
Twixenn <fiagustafsson1998@gmail.com>
2017-10-19 12:54:04 +0200

    Ändrat lite småsaker

--- components/praktik.php
+++ components/praktik.php
@@ -2,13 +2,13 @@
 <section id="praktik">
     <div class="container">
         <div class="row">
-            <div class="part col-md-6 col-sm-6">
+            <div class="part col-md-6 col-sm-12">
                 <h1>Lorem Ipsum</h1>
                 <div class="divider"></div>
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
             </div>
 
-            <div class="part form-right col-md-6 col-sm-6">
+            <div class="part form-right col-md-6 col-sm-12">
                 <form>
                     <input type="text" name="" value="" placeholder="Namn*">
                     <input type="text" name="" value="" placeholder="Företag*" required>
--- index.php
+++ index.php
@@ -1,8 +1,9 @@
 <!DOCTYPE html>
-<html>
+<html lang="se">
   <head>
     <meta charset="utf-8">
-    <title>Teknikum.it</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="bower_components/bootstrap/dist/css/bootstrap.min.css">
     <link href="style.css" rel="stylesheet" type="text/css"/>
     <script src="bower_components/jquery/dist/jquery.min.js" charset="utf-8"></script>
@@ -13,6 +14,7 @@
     <script src="components/js/highlightnav.js" charset="utf-8"></script>
     <script src="components/js/bildspel.js" type="text/javascript"></script>
     <script src="components/js/showimages.js" charset="utf-8"></script>
+    <title>Teknikum.it</title>
   </head>
   <body>
     <?php
@@ -20,7 +22,7 @@
       include "components/header.php";
       include "components/om.php";
       include "components/utbildning.php";
-      include "components/företag.php";
+      include "components/foretag.php";
       include "components/bilder.php";
       include "components/praktik.php";
       include "components/footer.php";
